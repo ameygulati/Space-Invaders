f0032b5a8f76df3f12d618585546a31998003440
Amey Gulati <ameygulati@gmail.com>
2024-01-20 19:41:15 -0800

    Working on moving character

--- space_invaders.py
+++ space_invaders.py
@@ -30,13 +30,30 @@ class Alien (Character):
             self.x += 1
         else:
             self.x -= 1
+
+class Player_ship(Character):
+    def __init__(self,x,y,image,length,width,dir_l,dir_r):
+        super().__init__(x,y,image,length,width)
+        self.dir_l=dir_l
+        self.dir_r=dir_r
+    
+    def right(self):
+        if x<600 and self.dir_r == True:
+            self.x+=1
+    def left(self):
+        if x > 0 and self.dir_l == True:
+            self.x-=1
+
+
 ltemp=[]
 l=[]
 x=1
 y=0
+move_r = False
+move_l = False
 for j in range(0,5,1):
     for i in range(0,10,1):
-        image_var=pygame.image.load("C:\\Users\\ameyg\\Space_Invaders\\enemy_1.png")
+        image_var=pygame.image.load("C:\\Users\\Amey\\Downloads\\Space Invaders\\Space-Invaders\\enemy_1.png")
         image_var=pygame.transform.scale(image_var,(20,20))
         ltemp.append(Alien(x,y,image_var,5,5,True))
         x+=35
@@ -44,16 +61,35 @@ for j in range(0,5,1):
     x=1
     l.append(ltemp)
 
+main = pygame.image.load("C:\\Users\\Amey\\Downloads\\Space Invaders\\Space-Invaders\\ship.png")
+main=pygame.transform.scale(main,(50,50))
+ship=Player_ship(250,500,main,50,50,move_l,move_r)
+
 while True:
     clock.tick(60)
     screen.fill((0,0,0))
+    ship.draw()
+    ship.right()
+    ship.left()
     for i in l:
         for j in i:
-            pygame.draw.rect(screen,(255,0,0),(j.x,j.y,20,20))
+            pygame.draw.rect(screen,(0,0,0),(j.x,j.y,20,20))
             j.draw()
             j.alien_move()
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             exit()
+        if event.type  == pygame.KEYDOWN:
+            if event.key == K_a:
+                move_l = True
+            elif event.key == K_d:
+                move_r = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == K_a:
+                move_l=False
+            elif event.key == K_d:
+                move_r=False
+
     pygame.display.update()
